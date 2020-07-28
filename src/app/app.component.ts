e0f3a55b0c2eeb270b794881f6526d3b62a1b4c3
Michael Hanft <michael.hanft@whiteduck.de>
2020-07-28 11:22:01 +0200

    show userData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   name = '...';
+  userData: any;
 
   async ngOnInit(): Promise<void> {
     const { name } = await (await fetch(`/api/name`)).json();
     this.name = name;
 
+    const { clientPrincipal } = await (await fetch(`/.auth/me`)).json();
+    this.userData = clientPrincipal;
   }
 
 }
